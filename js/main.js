2f74f0ca249165594712318471b82c9d0fb4a952
tubi55 <tubi55@nate.com>
2023-07-09 13:53:30 +0900

    이미지 소스 로딩완료 이벤트 추가

--- js/main.js
+++ js/main.js
@@ -3,6 +3,18 @@ const num = 200;
 //반환된 이미지를 전역변수에 담음
 const imgDOM = createImgs('figure', num);
 
+let count = 0;
+
+imgDOM.forEach((img) => {
+	img.onload = () => {
+		count++;
+		console.log(count);
+		if (count === num) {
+			console.log('이미지소스 로딩 완료');
+		}
+	};
+});
+
 //마우스 무브 이벤트 연결
 window.addEventListener('mousemove', (e) => matchMove(imgDOM, num, e));
 
@@ -23,3 +35,8 @@ function matchMove(arrEl, num, e) {
 	for (const img of arrEl) img.style.visibility = 'hidden';
 	arrEl[percent].style.visibility = 'visible';
 }
+
+//DOM객체가 생성된 직후 DOM에 수반되는 소스 자료들을 가져오기 시작
+//img요소는 DOM이 생성되어야지 그 이후에 이미지 소스를 불어옴
+//img.onload 이벤트를 연결하면 해당 돔에 수반되는 소스이미지가 완료되었을떄 호출떄
+//video.onloadeddata (영상소스 호출 이벤트)
