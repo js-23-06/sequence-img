6c11e9d8150e6c5f5313b6ec38f9019e86f774cb
tubi55 <tubi55@nate.com>
2023-08-13 14:42:29 +0900

    코드 정리

--- js/main.js
+++ js/main.js
@@ -1,35 +1,41 @@
 const num = 200;
-const mask = document.querySelector('aside');
-const delay = convertSpeed(mask);
-
+let count = 0;
 const imgDOM = createImgs('figure', num);
 
-let count = 0;
-
-imgDOM.forEach((img) => {
-	img.onload = () => {
-		count++;
-		const percent = parseInt((count / 200) * 100);
-		mask.querySelector('p').innerHTML = percent + '%';
-		mask.querySelector('.bar').style.width = percent + '%';
-		console.log(count);
-
-		if (count === num) {
-			console.log('이미지소스 로딩 완료');
-			mask.classList.add('off');
-
-			setTimeout(() => {
-				mask.remove();
-			}, delay);
-		}
-	};
-
-	img.onerror = (e) => {
-		e.currentTarget.setAttribute('src', 'img/logo.png');
-	};
-});
+showMask();
 
 window.addEventListener('mousemove', (e) => matchMove(imgDOM, num, e));
+
+function showMask() {
+	const mask = document.createElement('aside');
+	mask.style.transitionDuration = '0.5s';
+	const delay = convertSpeed(mask);
+	mask.innerHTML = `<p>0%</p><div class="bar"></div>`;
+	document.body.append(mask);
+
+	imgDOM.forEach((img) => {
+		img.onload = () => {
+			count++;
+			const percent = parseInt((count / 200) * 100);
+			mask.querySelector('p').innerHTML = percent + '%';
+			mask.querySelector('.bar').style.width = percent + '%';
+			console.log(count);
+
+			if (count === num) {
+				console.log('이미지소스 로딩 완료');
+				mask.classList.add('off');
+
+				setTimeout(() => {
+					mask.remove();
+				}, delay);
+			}
+		};
+
+		img.onerror = (e) => {
+			e.currentTarget.setAttribute('src', 'img/logo.png');
+		};
+	});
+}
 
 function createImgs(targetEl, num) {
 	const frame = document.querySelector(targetEl);
@@ -50,8 +56,3 @@ function matchMove(arrEl, num, e) {
 function convertSpeed(el) {
 	return parseFloat(getComputedStyle(el).transitionDuration) * 1000;
 }
-
-//DOM객체가 생성된 직후 DOM에 수반되는 소스 자료들을 가져오기 시작
-//img요소는 DOM이 생성되어야지 그 이후에 이미지 소스를 불어옴
-//img.onload 이벤트를 연결하면 해당 돔에 수반되는 소스이미지가 완료되었을떄 호출떄
-//video.onloadeddata (영상소스 호출 이벤트)
---
+++ js/sequence.js
@@ -0,0 +1,3 @@
+class Sequence {
+	constructor(selector) {}
+}
